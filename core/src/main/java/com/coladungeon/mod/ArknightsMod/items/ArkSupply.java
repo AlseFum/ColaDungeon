a36ffbf6422fc0175a66ab744d9b711fc7a0ed47
胡龄 <2652328248@qq.com>
2025-06-24 16:11:21 +0800

    minor:introduce trapist

--- core/src/main/java/com/coladungeon/mod/ArknightsMod/items/ArkSupply.java
+++ core/src/main/java/com/coladungeon/mod/ArknightsMod/items/ArkSupply.java
@@ -2,6 +2,8 @@ package com.coladungeon.mod.ArknightsMod.items;
 
 import com.coladungeon.items.supply.Supply;
 import com.coladungeon.mod.ArknightsMod.items.build.RhodesStandardSword;
+import com.coladungeon.mod.ArknightsMod.items.build.Specialist.TrappistWeapon.ExplosiveTrapItem;
+import com.coladungeon.mod.ArknightsMod.items.build.Specialist.TrappistWeapon.TrappistArtifact;
 import com.coladungeon.mod.ArknightsMod.items.build.caster.MechAccordWeapon;
 import com.coladungeon.mod.ArknightsMod.operator.Phantom.PhantomWeapon;
 import com.coladungeon.mod.ArknightsMod.operator.ProjektRed.RedKnife;
@@ -23,6 +25,8 @@ public class ArkSupply extends Supply {
             .put_in(MechAccordWeapon.class)
             .put_in(GuardSupply.class)
             .put_in(DefenderSupply.class)
+            .put_in(TrappistArtifact.class)
+            .put_in(ExplosiveTrapItem.class, 3)
             .name("罗德岛补给包")
             .desc("""
                 一个装满了罗德岛特制装备的补给包，可以从中获取到各种罗德岛特制装备。
@@ -37,6 +41,8 @@ public class ArkSupply extends Supply {
                 • 机械协奏武器 - 术师干员专用武器
                 • 守卫武器补给包 - 近卫干员武器集合
                 • 重装装甲补给包 - 重装干员装甲集合
+                • 陷阱师工具箱神器 - 制作爆炸陷阱的神器
+                • 爆炸陷阱 x3 - 可投掷的爆炸陷阱
                 """);
     }
 }
